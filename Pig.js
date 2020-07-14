f66f9c74680df7253a0757937c937eb9c815f481
Ekansh Sharma <53142357+teendevlper@users.noreply.github.com>
2020-07-14 16:38:42 +0530

    tint effect added and restart function added

---
+++ Pig.js
@@ -0,0 +1,20 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+   this.visibility = 255;
+    this.image = loadImage("sprites/enemy.png");
+  }
+display(){
+
+  if(this.body.speed < 2.5){
+    super.display();
+    }else{
+       push();
+       World.remove(world,this.body);
+       tint(255, this.visibility);
+       image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+       this.visibility = this.visibility - 5;
+       pop();
+     }
+  }
+};
